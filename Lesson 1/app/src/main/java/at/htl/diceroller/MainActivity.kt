7f51f6c8934f42ab6478d7fdb4f8c8d410da450b
Mario <mario.lengauer@gmx.net>
2019-09-28 15:48:01 +0200

    Step 21 completed

--- Lesson 1/app/src/main/java/at/htl/diceroller/MainActivity.kt
+++ Lesson 1/app/src/main/java/at/htl/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package at.htl.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
 
@@ -18,8 +18,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        var radomInt = Random.nextInt(6) + 1
-        resultText.text = radomInt.toString()
+
+        var randomInt = Random.nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
     }
 }

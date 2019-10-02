228ca1aacd6d935b96fb6df318306d9760430019
Mario <mario.lengauer@gmx.net>
2019-10-02 16:21:26 +0200

    Lesson 2, Steps 13

--- Lesson 2/app/src/main/java/at/htl/aboutme/MainActivity.kt
+++ Lesson 2/app/src/main/java/at/htl/aboutme/MainActivity.kt
@@ -2,11 +2,30 @@ package at.htl.aboutme
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener(){
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View){
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
     }
 }

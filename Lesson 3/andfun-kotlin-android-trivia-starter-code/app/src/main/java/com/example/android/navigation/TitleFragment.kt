7412db658140c7a17bc3553c8d470ce23d7427f7
Mario <mario.lengauer@gmx.net>
2019-10-10 19:52:00 +0200

    Lesson 3, Steps 8,9

--- Lesson 3/andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ Lesson 3/andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -8,6 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.example.android.navigation.R.id
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -18,6 +20,9 @@ class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
+        binding.playButton.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
         return binding.root
         }
     }
